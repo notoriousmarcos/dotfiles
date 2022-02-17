7d6e8d157f6395aa49fc7f0865e40e5e61faf710
Marcos Brito <mbrito@paypal.com>
2022-02-17 08:51:55 -0300

    Fixing brackets completions

--- nvim/after/plugin/autopairs.rc.lua
+++ nvim/after/plugin/autopairs.rc.lua
@@ -4,3 +4,16 @@ if (not status) then return end
 autopairs.setup({
   disable_filetype = { "TelescopePrompt" , "vim" },
 })
+
+autopairs.clear_rules()
+
+autopairs.add_rules {
+    Rule('{', '}')
+        :end_wise(function() return true end),
+
+    Rule('(', ')')
+        :end_wise(function() return true end),
+
+    Rule('[', ']')
+        :end_wise(function() return true end),
+}
