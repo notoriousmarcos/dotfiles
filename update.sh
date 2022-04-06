adf1c5c78d7e02f5ac170ec9b56c385c11dd70df
Marcos Brito <mbrito@paypal.com>
2022-04-06 09:14:34 -0300

    Adding sharable breakpoints too.

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 XCODE_THEME_DIR="${HOME}/Library/Developer/Xcode/UserData"
 cp -r "$XCODE_THEME_DIR/FontAndColorThemes" xcode 
 cp -r "$XCODE_THEME_DIR/CodeSnippets" xcode 
+cp -r "$XCODE_THEME_DIR/xcdebugger" xcode 
 cp -r "${HOME}/.config/nvim" .
 cp "${HOME}/.zshrc" .
 
